6696756bd9fd8e4a9f4ada2d8b4986248251c558
vm2124 <vm21242124@gmail.com>
2023-03-18 12:11:01 +0530

    done

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import "./Main.css";
 import newvm from "../../Assets/newvm.jpg";
 import { NavLink } from "react-router-dom";
-// import {BiPhoneCall} from "react-icons/bi"
-// import {AiFillLinkedin,AiFillGithub,AiOutlineMail} from "react-icons/ai"
+import gitr from '../../Assets/gitr.png'
+import lt from '../../Assets/lt.png'
+import gfg from '../../Assets/gfg.png'
 const Main = () => {
   return (
     <div className="Main">
@@ -24,6 +25,12 @@ const Main = () => {
 
         <button className="btn">Know More</button>
         </NavLink>
+        <div className="codepr">
+          <NavLink to={"https://github.com/vm21242124"}><img src={gitr} alt="" className="idd" /></NavLink>
+          <NavLink to={"https://leetcode.com/vm2124/"}><img src={lt} alt="" className="idd" /></NavLink>
+          <NavLink to={"https://auth.geeksforgeeks.org/user/vm21242124/"}><img src={gfg} alt="" className="idd" /></NavLink>
+
+        </div>
       </div>
       <div className="rightside">
         <img className="hero" src={newvm} alt="myimg" />
